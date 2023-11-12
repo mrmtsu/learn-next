683044bf750c6f6e39454fe2feb33fc186191fce
mrmtsu <81202muramatsu@gmail.com>
2023-11-13 07:33:20 +0900

    add hero components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import { Camp } from "@/components/Camp";
+import { Feature } from "@/components/Feature";
+import { GetApp } from "@/components/GetApp";
+import { Guide } from "@/components/Guide";
+import { Hero } from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Feature />
+      <GetApp />
+    </>
+  );
 }
---
+++ components/GetApp.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const GetApp = () => {
+  return <div>GetApp</div>;
+};
